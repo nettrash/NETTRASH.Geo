a7fe1d336a70282eb363de6e1a07957adeb8308f
NETTRASH <nettrash@nettrash.ru>
2020-05-20 14:26:41 +0300

    fix graph

--- Geo/GraphViewController.swift
+++ Geo/GraphViewController.swift
@@ -14,6 +14,8 @@ class GraphViewController : UIViewController {
 	private var graphPointsAltitudeBar: [Int] = []
 	private var graphPointsPressure: [Int] = []
 	private var graphPointsEverest: [Int] = []
+	private var maxDay: Date = Date()
+	private var minDay: Date = Date()
 
 	@IBOutlet var graphViewAltitudeBar: GraphView!
 	@IBOutlet var graphViewPressure: GraphView!
@@ -40,40 +42,60 @@ class GraphViewController : UIViewController {
 		let nMaxCount = 31
 		let app = UIApplication.shared.delegate as! AppDelegate
 		let moc = app.persistentContainer.viewContext
-		let traces: [Dictionary<String, Any>]? = try? moc.fetch(Trace.weekAggregateFetchRequest(nMaxCount)) as? [Dictionary<String, Any>]
-		var idx = traces?.count ?? 0
-		var cnt = 0
-		var pointsAltitudeBar: [Int] = []
-		var pointsPressure: [Int] = []
-		var pointsEverest: [Int] = []
-		for _ in 0..<nMaxCount {
-			pointsAltitudeBar.append(0)
-			pointsPressure.append(0)
-			pointsEverest.append(0)
-		}
-		while idx > 0 && cnt < nMaxCount {
-			idx -= 1
-			cnt += 1
-			let element = traces![idx]
-			pointsAltitudeBar[nMaxCount-cnt] = Int(element["maxAltitudeBAR"] as! Double)
-			pointsPressure[nMaxCount-cnt] = Int(element["minPressure"] as! Double)
-			pointsEverest[nMaxCount-cnt] = Int(element["maxEverest"] as! Double)
-		}
-		if cnt < nMaxCount && cnt > 0 {
-			let pab = pointsAltitudeBar[nMaxCount-cnt]
-			let pp = pointsPressure[nMaxCount-cnt]
-			let pe = pointsEverest[nMaxCount-cnt]
-			while cnt < nMaxCount {
-				cnt += 1
-				pointsAltitudeBar[nMaxCount-cnt] = pab
-				pointsPressure[nMaxCount-cnt] = pp
-				pointsEverest[nMaxCount-cnt] = pe
+		if let traces: [Dictionary<String, Any>] = try? moc.fetch(Trace.weekAggregateFetchRequest(nMaxCount)) as? [Dictionary<String, Any>] {
+		
+			maxDay = traces[0]["day"] as! Date
+			minDay = traces[(traces.count > nMaxCount ? nMaxCount - 1 : traces.count - 1)]["day"] as! Date
+			var idx = traces.count
+			var cnt = 0
+			var pointsAltitudeBar: [Int] = []
+			var pointsPressure: [Int] = []
+			var pointsEverest: [Int] = []
+			for _ in 0..<nMaxCount {
+				pointsAltitudeBar.append(0)
+				pointsPressure.append(0)
+				pointsEverest.append(0)
 			}
+			//for 100% day by day
+			/*
+			let calendar = Calendar.current
+			var day = Date()
+			let hour = calendar.component(.hour, from: day)
+			let minute = calendar.component(.minute, from: day)
+			let second = calendar.component(.second, from: day)
+			let nanosecond = calendar.component(.nanosecond, from: day)
+			day = calendar.date(byAdding: .nanosecond, value: -nanosecond, to: day)!
+			day = calendar.date(byAdding: .second, value: -second, to: day)!
+			day = calendar.date(byAdding: .minute, value: -minute, to: day)!
+			day = calendar.date(byAdding: .hour, value: -hour, to: day)!*/
+			while idx > 0 && cnt < nMaxCount {
+				idx -= 1
+				cnt += 1
+				/*if let element = traces?.first(where: { (item: Dictionary<String, Any>) -> Bool in
+					return item["day"] as! Date == day
+				}) {*/
+				let element = traces[idx]
+				pointsAltitudeBar[nMaxCount-cnt] = Int(element["maxAltitudeBAR"] as! Double)
+				pointsPressure[nMaxCount-cnt] = Int(element["minPressure"] as! Double)
+				pointsEverest[nMaxCount-cnt] = Int(element["maxEverest"] as! Double)
+				/*}
+				day = calendar.date(byAdding: .day, value: -1, to: day)!*/
+			}
+			if cnt < nMaxCount && cnt > 0 {
+				let pab = pointsAltitudeBar[nMaxCount-cnt]
+				let pp = pointsPressure[nMaxCount-cnt]
+				let pe = pointsEverest[nMaxCount-cnt]
+				while cnt < nMaxCount {
+					cnt += 1
+					pointsAltitudeBar[nMaxCount-cnt] = pab
+					pointsPressure[nMaxCount-cnt] = pp
+					pointsEverest[nMaxCount-cnt] = pe
+				}
+			}
+			graphPointsAltitudeBar = pointsAltitudeBar
+			graphPointsPressure = pointsPressure
+			graphPointsEverest = pointsEverest
 		}
-		graphPointsAltitudeBar = pointsAltitudeBar
-		graphPointsPressure = pointsPressure
-		graphPointsEverest = pointsEverest
-
 		#endif
 	}
 	
@@ -106,21 +128,21 @@ class GraphViewController : UIViewController {
 		graphViewAltitudeBar.lblAggregateTitle.text = NSLocalizedString("Average", comment: "")
 		graphViewAltitudeBar.lblAggregateValue.text = "\(average)"
 		
-		let today = Date()
+		/*let today = Date()
 		let calendar = Calendar.current
-		
+		*/
 		let formatter = DateFormatter()
-		formatter.setLocalizedDateFormatFromTemplate("EEEEE")
+		/*formatter.setLocalizedDateFormatFromTemplate("EEEEE")
 		
 		for i in 1...maxDayIndex {
 			if let date = calendar.date(byAdding: .day, value: -i, to: today),
 				let label = graphViewAltitudeBar.svLabelsY.arrangedSubviews[maxDayIndex - i] as? UILabel {
 				label.text = formatter.string(from: date)
 			}
-		}
+		}*/
 		
 		formatter.setLocalizedDateFormatFromTemplate("d LLL")
-		if let startDate = calendar.date(byAdding: .day, value: -maxDayIndex, to: today) {
+		/*if let startDate = calendar.date(byAdding: .day, value: -maxDayIndex, to: today) {
 			graphViewAltitudeBar.lblStartX.text = formatter.string(from: startDate)
 			let todayMonth = calendar.dateComponents([.month], from: today)
 			let startMonth = calendar.dateComponents([.month], from: startDate)
@@ -128,7 +150,9 @@ class GraphViewController : UIViewController {
 				formatter.setLocalizedDateFormatFromTemplate("d")
 			}
 		}
-		graphViewAltitudeBar.lblEndX.text = formatter.string(from: today)
+		graphViewAltitudeBar.lblEndX.text = formatter.string(from: today)*/
+		graphViewAltitudeBar.lblStartX.text = formatter.string(from: minDay)
+		graphViewAltitudeBar.lblEndX.text = formatter.string(from: maxDay)
 	}
 	
 	private func setupGraphPressure() {
@@ -160,21 +184,21 @@ class GraphViewController : UIViewController {
 		graphViewPressure.lblAggregateTitle.text = NSLocalizedString("Average", comment: "")
 		graphViewPressure.lblAggregateValue.text = "\(average)"
 		
-		let today = Date()
+		/*let today = Date()
 		let calendar = Calendar.current
-		
+		*/
 		let formatter = DateFormatter()
-		formatter.setLocalizedDateFormatFromTemplate("EEEEE")
+		/*formatter.setLocalizedDateFormatFromTemplate("EEEEE")
 		
 		for i in 1...maxDayIndex {
 			if let date = calendar.date(byAdding: .day, value: -i, to: today),
 				let label = graphViewPressure.svLabelsY.arrangedSubviews[maxDayIndex - i] as? UILabel {
 				label.text = formatter.string(from: date)
 			}
-		}
+		}*/
 		
 		formatter.setLocalizedDateFormatFromTemplate("d LLL")
-		if let startDate = calendar.date(byAdding: .day, value: -maxDayIndex, to: today) {
+		/*if let startDate = calendar.date(byAdding: .day, value: -maxDayIndex, to: today) {
 			graphViewPressure.lblStartX.text = formatter.string(from: startDate)
 			let todayMonth = calendar.dateComponents([.month], from: today)
 			let startMonth = calendar.dateComponents([.month], from: startDate)
@@ -182,7 +206,9 @@ class GraphViewController : UIViewController {
 				formatter.setLocalizedDateFormatFromTemplate("d")
 			}
 		}
-		graphViewPressure.lblEndX.text = formatter.string(from: today)
+		graphViewPressure.lblEndX.text = formatter.string(from: today)*/
+		graphViewPressure.lblStartX.text = formatter.string(from: minDay)
+		graphViewPressure.lblEndX.text = formatter.string(from: maxDay)
 	}
 	
 	private func setupGraphEverest() {
@@ -214,21 +240,21 @@ class GraphViewController : UIViewController {
 		graphViewEverest.lblAggregateTitle.text = NSLocalizedString("Average", comment: "")
 		graphViewEverest.lblAggregateValue.text = "\(average)"
 		
-		let today = Date()
+		/*let today = Date()
 		let calendar = Calendar.current
-		
+		*/
 		let formatter = DateFormatter()
-		formatter.setLocalizedDateFormatFromTemplate("EEEEE")
+		/*formatter.setLocalizedDateFormatFromTemplate("EEEEE")
 		
 		for i in 1...maxDayIndex {
 			if let date = calendar.date(byAdding: .day, value: -i, to: today),
 				let label = graphViewEverest.svLabelsY.arrangedSubviews[maxDayIndex - i] as? UILabel {
 				label.text = formatter.string(from: date)
 			}
-		}
+		}*/
 		
 		formatter.setLocalizedDateFormatFromTemplate("d LLL")
-		if let startDate = calendar.date(byAdding: .day, value: -maxDayIndex, to: today) {
+		/*if let startDate = calendar.date(byAdding: .day, value: -maxDayIndex, to: today) {
 			graphViewEverest.lblStartX.text = formatter.string(from: startDate)
 			let todayMonth = calendar.dateComponents([.month], from: today)
 			let startMonth = calendar.dateComponents([.month], from: startDate)
@@ -236,7 +262,9 @@ class GraphViewController : UIViewController {
 				formatter.setLocalizedDateFormatFromTemplate("d")
 			}
 		}
-		graphViewEverest.lblEndX.text = formatter.string(from: today)
+		graphViewEverest.lblEndX.text = formatter.string(from: today)*/
+		graphViewEverest.lblStartX.text = formatter.string(from: minDay)
+		graphViewEverest.lblEndX.text = formatter.string(from: maxDay)
 	}
 
 	override func viewDidLoad() {
